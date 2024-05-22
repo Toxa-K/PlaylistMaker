ce55d131c680847c52c0ae65c6bf5b773e83ad76
Toxa-K <timkov1506@gmail.com>
2024-05-22 05:45:14 +0300

    Created common style for the same elements Appropriate fonts were used Buttons on «Main screen» have no shadow The themes of the application are edited, when switching to night mode, the screens match the design.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val BackButton = findViewById<Button>(R.id.btn_settings_back)
+        val BackButton = findViewById<ImageView>(R.id.btn_settings_back)
         BackButton.setOnClickListener{
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
