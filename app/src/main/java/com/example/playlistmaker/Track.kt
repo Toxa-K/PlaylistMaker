e1f0954d38516ea26e84b22a6d4d4a1968e0393a
Toxa-K <timkov1506@gmail.com>
2024-07-24 22:51:35 +0300

    Созданы классы, описывающие элемент списка, ViewHolder и Adapter. Используется Glide, подключённый к проекту в виде библиотеки. Углы обложек треков закруглены. Ночной режим отобрaжается в соответствии с дизайном. Если на нет интернета, то показывается плейсхолдер.

---
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -0,0 +1,12 @@
+package com.example.playlistmaker;
+
+data class Track(
+        /*val name: String,*/
+
+        val trackName: String, // Название композиции
+        val artistName: String, // Имя исполнителя
+        val trackTime: String, // Продолжительность трека
+        val artworkUrl100: String, // Ссылка на изображение обложки
+
+)
+
