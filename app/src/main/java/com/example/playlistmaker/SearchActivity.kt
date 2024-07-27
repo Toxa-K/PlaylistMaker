1cfca33bf58b0735f1392704b6394f846ff69a5b
Toxa-K <timkov1506@gmail.com>
2024-07-27 11:48:16 +0300

    Название трека, имя исполнителя, обложка трека берутся при помощи iTunes Search API. Добавлены заглушки на ошибки в запросе клиента и на ошибки сервера. После того, как пользователь нажмёт на кнопку «Обновить», приложение  выполнить последний неудавшийся поисковый API запрос. Когда от сервера получен не пустой результат поискового запроса, то на экране «Поиск» отображается список треков, и пользователь видит элементы списка. Реализованно нажатие на кнопку «Очистить поисковый запрос».

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,124 +1,119 @@
 package com.example.playlistmaker
 
 import android.content.Context
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.TypedValue
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import kotlin.random.Random
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
 
-
-
+    private val iTunesBaseUrl = "https://itunes.apple.com"
     private var searchText: String = ""
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(SEARCH_TEXT_KEY, searchText)
-    }
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
-        findViewById<EditText>(R.id.search_input).setText(searchText)
-    }
+    private val iTunesService = retrofit.create(iTunesAPI::class.java)
+    private val tracks = ArrayList<Track>()
+    private val adapter = TrackAdapter(tracks)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        // Инициализация UI элементов
+        val searchInput = findViewById<EditText>(R.id.search_input) //Поле ввода
+        val clearButton = findViewById<ImageView>(R.id.clear_button)//Кнопка отчистки поля ввода
+        val backButton = findViewById<ImageView>(R.id.btn_settings_back)//Возврат на пред. страницу
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)//Список треков
+        val placeholderButton = findViewById<Button>(R.id.placeholderButton)//ТЕСТ ДЛЯ ПРОВЕРКИ
 
-        val searchInput = findViewById<EditText>(R.id.search_input)
-        val clearButton = findViewById<ImageView>(R.id.clear_button)
-        val backButton = findViewById<ImageView>(R.id.btn_settings_back)
-
-        //
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
 
-        val trackList: MutableList<Track> = mutableListOf()
 
-        trackList.add(
-            Track(
-                trackName = "Smells Like Teen Spirit",
-                artistName = "Nirvana",
-                trackTime = "5:01",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        trackList.add(
-            Track(
-                trackName = "Billie Jean",
-                artistName = "Michael Jackson",
-                trackTime = "4:35",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            )
-        )
-        trackList.add(
-            Track(
-                trackName = "Stayin' Alive",
-                artistName = "Bee Gees",
-                trackTime = "4:10",
-                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        trackList.add(
-            Track(
-                trackName = "Whole Lotta Love",
-                artistName = "Led Zeppelin",
-                trackTime = "5:33",
-                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            )
-        )
-        trackList.add(
-            Track(
-                trackName = "Sweet Child O'Mine",
-                artistName = "Guns N' Roses",
-                trackTime = "5:03",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        trackList.add(
-            Track(
-                trackName = "Sweet Child O'MineSweet Child O'MineSweet Child O'MineSweet Child O'MineSweet Child O'Mine",
-                artistName = "Guns N' Roses",
-                trackTime = "5:03",
-                artworkUrl100 = "a82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
+        // Установка слушателя для выполнения поиска при нажатии на клавиатуре "Done"
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // Выполнение поискового запроса
+                iTunesService.search(searchInput.text.toString()).enqueue(object :
+                    Callback<TrackResponse> {
+                        override fun onResponse(
+                            call: Call<TrackResponse>,
+                            response: Response<TrackResponse>
+                        ) {
+                            if (response.code() == 200) {
+                                tracks.clear()
+                                if (response.body()?.results?.isNotEmpty() == true) {
+                                    tracks.addAll(response.body()?.results!!)
+                                    adapter.notifyDataSetChanged()
+                                }
+                                if (tracks.isEmpty()) {
+                                    showMessage(getString(R.string.nothing_found), "")
+                                } else {
+                                    showMessage("", "")
+                                }
+                            } else {
+                                showMessage(getString(R.string.something_went_wrong),response.code().toString())
+                            }
+                        }
+                        override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                            showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                        }
+                    }
+                )
+                true
+            }
+            showMessage(getString(R.string.nothing_found), "")
+            false
+        }
 
-        val tracksAdapter = TrackAdapter(trackList)
-        recyclerView.adapter = tracksAdapter
+        //Принудительное прожатие "DONE" поля ввода
+        placeholderButton.setOnClickListener{
+            searchInput.onEditorAction(EditorInfo.IME_ACTION_DONE)
+        }
 
 
         if (savedInstanceState != null) {
             searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
             searchInput.setText(searchText)
         }
+
+
+        // Установка слушателя для кнопки очистки поля ввода
         clearButton.setOnClickListener {
             searchInput.setText("")
             hideKeyboard(searchInput)
-
+            tracks.clear() // Очистка списка треков
+            adapter.notifyDataSetChanged() // Уведомление адаптера об изменении данных
         }
+
+
+        // Установка слушателя для кнопки назад
         backButton.setOnClickListener{
             finish()
         }
+
+
         // логика по работе с введённым значением
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -132,23 +127,70 @@ class SearchActivity : AppCompatActivity() {
                 // empty
             }
         }
-
         searchInput.addTextChangedListener(simpleTextWatcher)
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT_KEY, searchText)
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+        findViewById<EditText>(R.id.search_input).setText(searchText)
+    }
+
+
+    // Скрытие клавиатуры
     private fun hideKeyboard(view: View) {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
+
+    // Определение видимости кнопки очистки
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
         } else {
             View.VISIBLE
         }
-
     }
+
+
     private companion object {
         const val SEARCH_TEXT_KEY = "SEARCH_TEXT_KEY"
+    }
+
+
+    // Отображение сообщения пользователю ошибки
+    private fun showMessage(text: String, additionalMessage: String) {
+        val placeholderMessage = findViewById<TextView>(R.id.placeholderMessage)
+        val placeholderButton = findViewById<Button>(R.id.placeholderButton)
+        val placeholderIcon = findViewById<ImageView>(R.id.placeholderIcon)
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            placeholderButton.visibility = View.GONE
+            if (additionalMessage.isNotEmpty()) {//Отсутствие интернета
+                placeholderIcon.setImageResource(R.drawable.off_ethernet_search)
+                placeholderIcon.visibility = View.VISIBLE
+                placeholderButton.visibility = View.VISIBLE
+            }
+            else{//Отсутствие треков
+                placeholderIcon.setImageResource(R.drawable.none_search)
+                placeholderIcon.visibility = View.VISIBLE
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+            placeholderButton.visibility = View.GONE
+            placeholderIcon.visibility = View.GONE
+        }
     }
 }
 
